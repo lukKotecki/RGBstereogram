353e3f571a689182e8185b6ff0af90d63c9e86f8
Lukas <lukkotecki@gmail.com>
2024-04-09 14:46:49 +0200

    handling image display update

--- src/components/InputImages.jsx
+++ src/components/InputImages.jsx
@@ -4,102 +4,49 @@ export default function InputImages({ inputsState, setInputsState}){
 
     console.log(inputsState)
 
-    const canvasRef = React.useRef(null)
-    
-    // React.useEffect(()=>{
-    //     const canvas = canvasRef.current
-    //     const context = canvas.getContext('2d')
-    //     // context.fillStyle = '#123123'
-        
-    //     const image = new Image()
-    //     let URL = window.URL
-    //     if(inputsState.secondImageUrl){
-    //         image.src = URL.createObjectURL(inputsState.secondImageUrl)
-    //         context.drawImage(image, 0, 0, canvas.width, canvas.height)
-    //     }
-
-    // },[inputsState])
-
-
-    // input.addEventListener('change',(e)=>{
-    //     const image = new Image()
-    //     let URL = window.URL;
-    //     let url = URL.createObjectURL(e.target.files[0]);
-    //     image.src = url;
-    
-    //     image.addEventListener('load', ()=>{
-    //         ctx.drawImage(image, 0, 0, 100,100)
-    //         // ctx.drawImage(image, 0, 0)
-    //     })
-    // })
-
     function handleInput(e){
         e.preventDefault()
+        console.log("To jest otrzymane: "+e.target.type)
 
-        // console.log(typeof e.target.files[0])
-        //image.src = url;
-
-        // let url = URL.createObjectURL(e.target.files[0]);
-        // console.log(url)
-
-        // if(e.target.value){
-        //     setInputsState(prev=> ({...prev, [e.target.id]: e.target.value}))
-        // }else if(e.target.files[0]){
-        //     // let URL = window.URL;
-        //     let url = URL.createObjectURL(e.target.files[0]);
-        //     setInputsState(prev=>({
-        //         ...prev, [e.target.id]: url
-        //     }))
-        // }
-        const URL = window.URL
-        const url = URL.createObjectURL(e.target.files[0])
-        setInputsState(prev=>({
-            ...prev,
-            [e.target.id]: url
-        }))
+        if(e.target.type === 'number'){
+            setInputsState(prev=> ({...prev, [e.target.id]: e.target.value }))
+        }else if(e.target.type === 'file'){
+            const url = window.URL.createObjectURL(e.target.files[0])
+            setInputsState(prev=>({ ...prev, [e.target.id]: url}))
+            handleImageUpload(e)
+        }
+        
+    }
+    
+    function handleImageUpload(e){
+        console.log(e.target.files.length)
+        switch(e.target.id){
+            case 'firstImageUrl':
+                console.log('kliknięto w pierwszym, ilość: '+e.target.files.length)
+        }
 
     }
 
     return (
-        <form id='form-container' onChange={handleInput}>
+        <form id='form-container'>
             <div className='input-wrapper'>
-                <div className='canvas-wrapper'>
-                    {/* <canvas /> */}
-                    <img src={inputsState.firstImageUrl}/>
-                    <br/>
-                    <label htmlFor="firstImageUrl" className="custom-file-upload">
-                        Upload file
-                    </label>
-                    <input id="firstImageUrl" type="file" />
-                    <canvas ref={canvasRef} id='canvas' >fota</canvas>
-                    <div className='number-inputs-wrapper'>
-                        <label htmlFor='firstRed'>Red: </label>
-                        <input onChange={handleInput} id='firstRed' name='firstRed' type='number' min='0' max='100' value={inputsState.firstRed}></input>
-                        <label htmlFor='firstGreen'>Green: </label>
-                        <input onChange={handleInput} id='firstGreen' name='firstGreen' type='number' min='0' max='100' value={inputsState.firstGreen}></input>
-                        <label htmlFor='firstBlue'>Blue: </label>
-                        <input onChange={handleInput} id='firstBlue' name='firstBlue' type='number' min='0' max='100' value={inputsState.firstBlue}></input>
-                    </div>
-                </div>
-
-                <div className='canvas-wrapper'>
-                    {/* <canvas /> */}
-                    <img src={inputsState.secondImageUrl}/>
-                    <br/>
-                    <label htmlFor="secondImageUrl" className="custom-file-upload">
-                        Upload file
-                    </label>
-                    <input id="secondImageUrl" type="file" />
-                    <div className='number-inputs-wrapper'>
-                        <label htmlFor='second-red'>Red: </label>
-                        <input id='second-red' name='second-red' type='number' min='0' max='100'></input>
-                        <label htmlFor='second-green'>Green: </label>
-                        <input id='second-green' name='second-green' type='number' min='0' max='100'></input>
-                        <label htmlFor='second-blue'>Blue: </label>
-                        <input id='second-blue' name='second-blue' type='number' min='0' max='100'></input>
-                    </div>
+                <img src={inputsState.firstImageUrl}/>
+                <br/>
+                <label htmlFor="firstImageUrl" className="custom-file-upload">
+                    Upload file
+                </label>
+                <input onChange={handleInput} id="firstImageUrl" type="file" multiple />
+                <div className='number-inputs-wrapper'>
+                    <label htmlFor='firstRed'>Red: </label>
+                    <input onChange={handleInput} id='firstRed' name='firstRed' type='number' min='0' max='100' value={inputsState.firstRed}></input>
+                    <label htmlFor='firstGreen'>Green: </label>
+                    <input onChange={handleInput} id='firstGreen' name='firstGreen' type='number' min='0' max='100' value={inputsState.firstGreen}></input>
+                    <label htmlFor='firstBlue'>Blue: </label>
+                    <input onChange={handleInput} id='firstBlue' name='firstBlue' type='number' min='0' max='100' value={inputsState.firstBlue}></input>
                 </div>
             </div>
+
+
 
             <div className='input-wrapper'>
                 <div className="number-inputs-wrapper">
