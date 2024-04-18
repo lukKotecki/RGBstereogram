874ad5d6d1b2c2f360553d4a8e1aa7aaa3d582a9
Lukas <lukkotecki@gmail.com>
2024-04-18 20:54:22 +0200

    images zebra pattern beta

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Footer from './components/Footer'
 import InputImages from './components/InputImages'
 import OutputImage from './components/OutputImage'
 import whiteNoiseImage from './assets/images/white_noise.png'
+import boyImage from './assets/images/boy-image.jpg'
+import dogImage from './assets/images/dog-image.jpeg'
 
 function App() {
 
@@ -17,14 +19,14 @@ function App() {
     }, 
     {
       imageId: 'secondImage',
-      imageUrl: whiteNoiseImage,
+      imageUrl: boyImage,
       red: 3,
       green: 77,
       blue: 4 
     }, 
     {
       imageId: 'thirdImage',
-      imageUrl: whiteNoiseImage,
+      imageUrl: dogImage,
       red: 5,
       green: 10,
       blue: 6 
@@ -39,10 +41,7 @@ function App() {
   const secondCanvasRef = React.useRef(null)
   const thirdCanvasRef = React.useRef(null)
 
-
   function createOutput(){
-    console.log('odpalam funkcje w rodzicu')
-
     const outputCanvas = outputCanvasRef.current
     const outputCtx = outputCanvas.getContext('2d')
     const firstCanvas = firstCanvasRef.current
@@ -52,8 +51,6 @@ function App() {
     const thirdCanvas = thirdCanvasRef.current
     const thirdCtx = thirdCanvas.getContext('2d')
 
-
-
     const outputImage = new Image()
     // outputImage.src = inputsState[0].imageUrl
     // outputCtx.drawImage(outputImage, 0, 0, outputCanvas.width, outputCanvas.height)
@@ -62,49 +59,52 @@ function App() {
     firstImage.src = inputsState[0].imageUrl
     firstCtx.drawImage(firstImage, 0, 0, outputCanvas.width, outputCanvas.height)
     const firstData = firstCtx.getImageData(0,0, outputCanvas.width, outputCanvas.height).data
+
     const secondImage = new Image()
     secondImage.src = inputsState[1].imageUrl
-    secondCtx.drawImage(outputImage, 0, 0, outputCanvas.width, outputCanvas.height)
+    secondCtx.drawImage(secondImage, 0, 0, outputCanvas.width, outputCanvas.height)
     const secondData =secondCtx.getImageData(0,0, outputCanvas.width, outputCanvas.height).data
+
     const thirdImage = new Image()
     thirdImage.src = inputsState[2].imageUrl
     thirdCtx.drawImage(thirdImage, 0, 0, outputCanvas.width, outputCanvas.height)
     const thirdData = thirdCtx.getImageData(0,0, outputCanvas.width, outputCanvas.height).data
 
-
-    // const imageData = outputCtx.getImageData(0,0, outputCanvas.width, outputCanvas.height)
-    // const data = imageData.data
-    // console.log(data)
-    // for (let i = 0; i < data.length; i += 4) {
-    //     const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-    //     data[i] = avg; // red
-    //     data[i + 1] = avg; // green
-    //     data[i + 2] = avg; // blue
-    //   }
-    // outputCtx.putImageData(imageData, 0, 0);
-
-    
-    let imageData = firstCtx.getImageData(0, 0, firstCanvas.width, firstCanvas.height);
+    let imageData = secondCtx.getImageData(0, 0, firstCanvas.width, firstCanvas.height);
     let data = imageData.data;
 
-
-    for(let i=0; i<100000; i+=4){
-      data[i] = 0;     // Red
-      data[i + 1] = 255;   // Green
-      data[i + 2] = 0;   // Blue
-      data[i + 3] = 255; // Alpha
+    for(let i=0, flipFlap = 0; i<data.length; i+=4, flipFlap++ ){
+      if(flipFlap === 0 ){
+        data[i] = thirdData[i];     // Red
+        data[i + 1] = thirdData[i+1];   // Green
+        data[i + 2] = thirdData[i+2];   // Blue
+        data[i + 3] =  thirdData[i+3]; // Alpha
+      }
+      if(flipFlap === 1) {
+        data[i] = firstData[i];     // Red
+        data[i + 1] = firstData[i+1];   // Green
+        data[i + 2] = firstData[i+2];   // Blue
+        data[i + 3] =  firstData[i+3]; // Alpha
+      }
+      if(flipFlap === 2) {
+        data[i] = secondData[i];     // Red
+        data[i + 1] = secondData[i+1];   // Green
+        data[i + 2] = secondData[i+2];   // Blue
+        data[i + 3] =  secondData[i+3]; // Alpha
+      }
+      if(flipFlap === 2){
+        flipFlap = -1 
+      }
     }
 
     outputCtx.putImageData(imageData, 0, 0)
 
   }
 
-
   const getColorIndicesForCoord = (x, y, width) => {
     const red = y * (width * 4) + x * 4;
     return [red, red + 1, red + 2, red + 3];
   };
-
 
 return (
     <div className='container'>
@@ -115,9 +115,9 @@ return (
       
       {/* {canvas} */}
       <canvas ref={outputCanvasRef} id='outputImage'></canvas>
-      <canvas style={{display:'none'}} ref={firstCanvasRef}></canvas>
-      <canvas style={{display:'none'}} ref={secondCanvasRef}></canvas>
-      <canvas style={{display:'none'}} ref={thirdCanvasRef}></canvas>
+      <canvas style={{display:'initial'}} ref={firstCanvasRef}></canvas>
+      <canvas style={{display:'initial'}} ref={secondCanvasRef}></canvas>
+      <canvas style={{display:'initial'}} ref={thirdCanvasRef}></canvas>
       <Footer />
     </div>
   )
